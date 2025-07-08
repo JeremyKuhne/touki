ede89850678a2f165816f94b325f387c092a2e43
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-07-07 22:07:08 -0700

    Improved .NET Framework string hashcode perf.
    It was significantly slower for very long strings. Now it is about the same for 100 characters and up.

---
+++ touki.tests/Touki/StringsTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2025 Jeremy W Kuhne
+// SPDX-License-Identifier: MIT
+// See LICENSE file in the project root for full license information
+
+namespace Touki;
+
+public class StringsTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("Aar")]
+    [InlineData("The quick brown fox jumps over the lazy dog.")]
+    public void GetHashCode_EqualsString(string? value)
+    {
+        ReadOnlySpan<char> span = value.AsSpan();
+        value ??= "";
+
+        int hash1 = Strings.GetHashCode(span);
+        int hash2 = value.GetHashCode();
+
+        hash1.Should().Be(hash2);
+    }
+
+    [Fact]
+    public void GetHashCode_EqualsString_Sliced()
+    {
+        string test = "The quick brown fox jumps over the lazy dog.";
+        ReadOnlySpan<char> span = test.AsSpan()[11..];
+
+        int hash1 = Strings.GetHashCode(span);
+        int hash2 = span.ToString().GetHashCode();
+
+        hash1.Should().Be(hash2);
+    }
+}
--- touki/Touki/Text/Strings.cs
+++ touki/Touki/Text/Strings.cs
@@ -102,40 +102,71 @@ public static partial class Strings
     /// <summary>
     ///  Generates a hash code for the specified string value that matches what <see langword="string"/> generates.
     /// </summary>
-    public static int GetHashCode(ReadOnlySpan<char> value)
+    /// <remarks>
+    ///  <para>
+    ///   On .NET Framework strings don't go beyond embedded nulls when calculating hash codes. If this matters to
+    ///   you, you'll need to slice the span to the first null character. In addition, this is not a safe hashing
+    ///   algorithm, it is not resistant to hash collisions, and should not be used for security purposes. It is meant
+    ///   to give you a hash code that matches what <see langword="string"/> generates for the same value.
+    ///  </para>
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe int GetHashCode(ReadOnlySpan<char> value)
     {
 #if NET
         return string.GetHashCode(value);
 #else
+        // .NET Framework uses the DJB2 (Daniel J. Bernstein) algorithm. It iterates through to the first null character.
+        // Here we don't know if we'll have one so we use the length and unroll to get the next best thing. The speed
+        // converges on rough equivalence with about 100 characters and above. At smaller sizes there is about a
+        // 5ns overhead penalty.
+
         if (value.IsEmpty)
         {
             // "".GetHashCode();
             return 371857150;
         }
 
-        // This is the 64bit .NET Framework implementation
-        // adapted to work with spans instead of pointers
-        int hash1 = 5381;
-        int hash2 = hash1;
-
-        int i = 0;
-        int length = value.Length;
-
-        while (i < length)
+        fixed (char* ptr = value)
         {
-            int c = value[i];
-            hash1 = ((hash1 << 5) + hash1) ^ c;
-            i++;
+            // For strings 10-100+ chars, unrolling by 4 provides best performance
+            int hash1 = 5381;
+            int hash2 = hash1;
 
-            if (i < length)
+            char* p = ptr;
+            int remaining = value.Length;
+
+            // Process 4 characters at a time
+            while (remaining >= 4)
             {
-                c = value[i];
-                hash2 = ((hash2 << 5) + hash2) ^ c;
-                i++;
-            }
-        }
+                hash1 = ((hash1 << 5) + hash1) ^ p[0];
+                hash2 = ((hash2 << 5) + hash2) ^ p[1];
+                hash1 = ((hash1 << 5) + hash1) ^ p[2];
+                hash2 = ((hash2 << 5) + hash2) ^ p[3];
 
-        return hash1 + (hash2 * 1566083941);
+                p += 4;
+                remaining -= 4;
+            }
+
+            // Handle remaining characters
+            if (remaining == 3)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ p[0];
+                hash2 = ((hash2 << 5) + hash2) ^ p[1];
+                hash1 = ((hash1 << 5) + hash1) ^ p[2];
+            }
+            else if (remaining == 2)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ p[0];
+                hash2 = ((hash2 << 5) + hash2) ^ p[1];
+            }
+            else if (remaining == 1)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ p[0];
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
 #endif
     }
 }
