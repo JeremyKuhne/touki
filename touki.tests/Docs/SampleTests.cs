012a986a116c78bf99c38504bb145d9ec42b7c87
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-07-23 15:22:16 -0700

    Enhance documentation around string handling (#48)
    - Updated README.md with new "Overviews" section with link to string overview.
    - Introduced strings.md to detail performance benefits of the Touki library for string handling.
    - Added Box method to Value struct for boxing unsupported types and clarified storage strategy.

---
+++ touki.tests/Docs/SampleTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2025 Jeremy W Kuhne
+// SPDX-License-Identifier: MIT
+// See LICENSE file in the project root for full license information
+
+using Touki.IO;
+
+namespace Touki.Docs;
+
+public class SampleTests
+{
+    [Fact]
+    public void StringSegment_Indexing()
+    {
+        string csv = "apple,banana,cherry";
+        StringSegment full = new(csv);
+        int comma = full.IndexOf(',');
+        StringSegment first = full[..comma];
+
+        first.Should().Be("apple");
+
+        List<string> segments = [];
+
+        StringSegment right = full;
+        while (right.TrySplit(',', out StringSegment left, out right))
+        {
+            // left will be "apple", "banana", "cherry" in each iteration
+            segments.Add(left.ToString());
+        }
+
+        segments.Should().BeEquivalentTo(["apple", "banana", "cherry"]);
+    }
+
+    [Fact]
+    public void Value_Demo()
+    {
+        Value[] args = [1, 2.5, "three"];
+        string fmt = "{0} - {1} - {2}";
+        string result = Strings.Format(fmt, args);
+        result.Should().Be("1 - 2.5 - three");
+    }
+
+    [Fact]
+    public void StreamExtension()
+    {
+        string name = "Name";
+        int version = 12;
+
+        using MemoryStream stream = new();
+        stream.WriteFormatted($"Library: {name}, Version: {version}");
+    }
+}
---
+++ touki/Framework/Touki/Io/StreamExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2025 Jeremy W Kuhne
+// SPDX-License-Identifier: MIT
+// See LICENSE file in the project root for full license information
+
+using System.Buffers;
+using System.IO;
+
+namespace Touki.IO;
+
+/// <summary>
+///  Extension methods for <see cref="Stream"/>.
+/// </summary>
+public static partial class StreamExtensions
+{
+    /// <summary>
+    ///  Allows writing a <see cref="ReadOnlySpan{Char}"/> to a <see cref="TextWriter"/>.
+    /// </summary>
+    public static void Write(this TextWriter writer, ReadOnlySpan<char> value)
+    {
+        if (value.Length > 0)
+        {
+            char[] buffer = ArrayPool<char>.Shared.Rent(value.Length);
+            value.CopyTo(buffer);
+            writer.Write(buffer, 0, value.Length);
+            ArrayPool<char>.Shared.Return(buffer);
+        }
+    }
+
+    /// <summary>
+    ///  Allows writing a <see cref="ReadOnlySpan{Char}"/> to a <see cref="TextWriter"/>.
+    /// </summary>
+    public static void WriteLine(this TextWriter writer, ReadOnlySpan<char> value)
+    {
+        if (value.Length > 0)
+        {
+            char[] buffer = ArrayPool<char>.Shared.Rent(value.Length);
+            value.CopyTo(buffer);
+            writer.Write(buffer, 0, value.Length);
+            ArrayPool<char>.Shared.Return(buffer);
+        }
+
+        writer.WriteLine();
+    }
+}
