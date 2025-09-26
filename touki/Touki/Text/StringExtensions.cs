6d387d952484fb91a3113482c3afbc6e92379b18
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-09-26 14:26:10 -0700

    Whoops

--- touki/Touki/Text/StringExtensions.cs
+++ touki/Touki/Text/StringExtensions.cs
@@ -2,10 +2,6 @@
 // SPDX-License-Identifier: MIT
 // See LICENSE file in the project root for full license information
 
-// Copyright (c) 2025 Jeremy W Kuhne
-// SPDX-License-Identifier: MIT
-// See LICENSE file in the project root for full license information
-
 namespace Touki.Text;
 
 /// <summary>
