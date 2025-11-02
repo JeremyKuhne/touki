d16bd5a2a20ac28d9232dd01d59e1b54cdcb6099
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-11-02 10:20:44 -0800

    Add string Concat and ReplaceLineEndings
    Adds additional Concat overloads and ReplaceLineEndings for .NET Framework.

--- touki/Framework/Touki/Text/StringExtensions.cs
+++ touki/Framework/Touki/Text/StringExtensions.cs
@@ -167,5 +167,84 @@ public static partial class StringExtensions
             stringValue.AsSpan().CopyTo(destination);
             return true;
         }
+
+        /// <inheritdoc cref="Concat(ReadOnlySpan{char}, ReadOnlySpan{char}, ReadOnlySpan{char}, ReadOnlySpan{char})"/>
+        public static unsafe string Concat(ReadOnlySpan<char> str0, ReadOnlySpan<char> str1)
+        {
+            int length = checked(str0.Length + str1.Length);
+            if (length == 0)
+            {
+                return string.Empty;
+            }
+
+            string result = FastAllocateString(length);
+            fixed (char* firstChar = result)
+            {
+                Span<char> resultSpan = new(firstChar, result.Length);
+                str0.CopyTo(resultSpan);
+                str1.CopyTo(resultSpan[str0.Length..]);
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc cref="Concat(ReadOnlySpan{char}, ReadOnlySpan{char}, ReadOnlySpan{char}, ReadOnlySpan{char})"/>
+        public static unsafe string Concat(ReadOnlySpan<char> str0, ReadOnlySpan<char> str1, ReadOnlySpan<char> str2)
+        {
+            int length = checked(str0.Length + str1.Length + str2.Length);
+            if (length == 0)
+            {
+                return string.Empty;
+            }
+
+            string result = FastAllocateString(length);
+            fixed (char* firstChar = result)
+            {
+                Span<char> resultSpan = new(firstChar, result.Length);
+                str0.CopyTo(resultSpan);
+                resultSpan = resultSpan[str0.Length..];
+                str1.CopyTo(resultSpan);
+                resultSpan = resultSpan[str1.Length..];
+                str2.CopyTo(resultSpan);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///  Concatenates the string representations of the specified read-only character spans.
+        /// </summary>
+        /// <param name="str0">The first span to concatenate.</param>
+        /// <param name="str1">The second span to concatenate.</param>
+        /// <param name="str2">The third span to concatenate.</param>
+        /// <param name="str3">The fourth span to concatenate.</param>
+        /// <returns>The concatenated string.</returns>
+        public static unsafe string Concat(
+            ReadOnlySpan<char> str0,
+            ReadOnlySpan<char> str1,
+            ReadOnlySpan<char> str2,
+            ReadOnlySpan<char> str3)
+        {
+            int length = checked(str0.Length + str1.Length + str2.Length + str3.Length);
+            if (length == 0)
+            {
+                return string.Empty;
+            }
+
+            string result = FastAllocateString(length);
+            fixed (char* firstChar = result)
+            {
+                Span<char> resultSpan = new(firstChar, result.Length);
+                str0.CopyTo(resultSpan);
+                resultSpan = resultSpan[str0.Length..];
+                str1.CopyTo(resultSpan);
+                resultSpan = resultSpan[str1.Length..];
+                str2.CopyTo(resultSpan);
+                resultSpan = resultSpan[str2.Length..];
+                str3.CopyTo(resultSpan);
+            }
+
+            return result;
+        }
     }
 }
