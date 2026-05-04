79091373b78fe59d0efa25bd5b31ae673a5edacc
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2026-05-04 10:05:32 -0700

    Reorganize Framework: move polyfills into Polyfills/System.Text/
    Encoding span overloads, StringBuilder.ChunkEnumerator, StringBuilder.Append(ROS<char>) extensions move to namespace System.Text. char.IsAsciiLetter polyfill moves to namespace System under Polyfills/System/CharExtensions.cs.

--- touki/Framework/Polyfills/System/CharExtensions.cs
+++ touki/Framework/Polyfills/System/CharExtensions.cs
@@ -1,8 +1,10 @@
-﻿// Copyright (c) 2025 Jeremy W Kuhne
+// Copyright (c) 2025 Jeremy W Kuhne
 // SPDX-License-Identifier: MIT
 // See LICENSE file in the project root for full license information
 
-namespace Touki.Text;
+using Touki;
+
+namespace System;
 
 public static partial class CharExtensions
 {
