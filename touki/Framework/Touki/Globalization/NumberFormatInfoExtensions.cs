d4e1a0ee3005e5775d8305eabbffdc8c767466a0
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2026-05-04 10:27:51 -0700

    Reorganize Framework: clean up Touki-folder leftovers
    BitConverters.cs (BitConverterExtensions) moves to Polyfills/System/ namespace System -- it polyfills BCL BitConverter members.
    
    TypeExtensions.cs moves to Polyfills/System/ namespace System -- it polyfills Type.IsAssignableTo (BCL .NET 5+).
    
    Touki/StringExtensions.CopyTo(Span<char>) (PR #107 polyfill of string.CopyTo from .NET Core 2.1+) merges into Polyfills/System/StringExtensions.cs as an extension(string source) block.
    
    EnumExtensions stays in namespace Touki because it shares a partial class with internal EnumData consumed by Touki.Text.ValueStringBuilder; was briefly moved before the cross-namespace partial-class break was discovered.
    
    NumberFormatInfoExtensions moves to Touki/Globalization/ folder (namespace Touki.Globalization) -- reflects into BCL internals so stays in Touki per user direction. Number.Formatting.cs adds 'using Touki.Globalization;' to follow the move.
    
    Removes empty touki/Framework/System tree.

--- touki/Framework/Touki/Globalization/NumberFormatInfoExtensions.cs
+++ touki/Framework/Touki/Globalization/NumberFormatInfoExtensions.cs
@@ -1,11 +1,11 @@
-﻿// Copyright (c) 2025 Jeremy W Kuhne
+// Copyright (c) 2025 Jeremy W Kuhne
 // SPDX-License-Identifier: MIT
 // See LICENSE file in the project root for full license information
 
 using System.Globalization;
 using System.Reflection;
 
-namespace Touki;
+namespace Touki.Globalization;
 
 internal static class NumberFormatInfoExtensions
 {
